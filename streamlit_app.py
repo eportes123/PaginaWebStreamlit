04f8da58be2c414d4f613760714c25df7af9d152
eportes123 <152937728+eportes123@users.noreply.github.com>
2023-12-04 20:28:36 -0600

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
-pip install -q pycountry
-pip install -q streamlit_option_menu
-pip install -q streamlit
+install pycountry
+install streamlit_option_menu
+install streamlit
 
 import streamlit as st
 import pandas as pd
